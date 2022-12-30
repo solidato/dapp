79daa3a30321af95e22c660e33618a34554e6946
Stefano Ceschi Berrini <stefano.ceschib@gmail.com>
2022-12-30 16:38:29 +0100

    chore: prettier test

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,31 +1,32 @@
 import React, { useMemo } from "react";
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import HomeIcon from '@mui/icons-material/Home';
-import ListItemText from '@mui/material/ListItemText';
-import SettingsIcon from '@mui/icons-material/Settings';
-import TaskIcon from '@mui/icons-material/Task';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import HomeIcon from "@mui/icons-material/Home";
+import ListItemText from "@mui/material/ListItemText";
+import SettingsIcon from "@mui/icons-material/Settings";
+import TaskIcon from "@mui/icons-material/Task";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 import { IconButton, Theme } from "@mui/material";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
 const drawerWidth = 240;
 
-const initActiveStyle = (currentPath: string) => (href: string) => currentPath === href ? { backgroundColor: (theme: Theme) => theme.palette.grey[300] } : {}
+const initActiveStyle = (currentPath: string) => (href: string) =>
+  currentPath === href ? { backgroundColor: (theme: Theme) => theme.palette.grey[300] } : {};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const router = useRouter();
-  const getActiveStyle = useMemo(() => initActiveStyle(router.asPath), [router.asPath])
+  const getActiveStyle = useMemo(() => initActiveStyle(router.asPath), [router.asPath]);
 
   const [mobileOpen, setMobileOpen] = React.useState(false);
 
@@ -39,7 +40,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <Divider />
       <List>
         <ListItem disablePadding>
-          <ListItemButton component={Link} href="/" sx={getActiveStyle('/')}>
+          <ListItemButton component={Link} href="/" sx={getActiveStyle("/")}>
             <ListItemIcon>
               <HomeIcon />
             </ListItemIcon>
@@ -47,7 +48,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           </ListItemButton>
         </ListItem>
         <ListItem disablePadding>
-          <ListItemButton component={Link} href="/tasks" sx={getActiveStyle('/tasks')}>
+          <ListItemButton component={Link} href="/tasks" sx={getActiveStyle("/tasks")}>
             <ListItemIcon>
               <TaskIcon />
             </ListItemIcon>
@@ -58,7 +59,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <Divider />
       <List>
         <ListItem disablePadding>
-          <ListItemButton component={Link} href="/settings" sx={getActiveStyle('/settings')}>
+          <ListItemButton component={Link} href="/settings" sx={getActiveStyle("/settings")}>
             <ListItemIcon>
               <SettingsIcon />
             </ListItemIcon>
@@ -69,10 +70,10 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     </div>
   );
 
-  const container = typeof window !== 'undefined' ? document.body : undefined;
+  const container = typeof window !== "undefined" ? document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
       <AppBar
         position="fixed"
@@ -87,7 +88,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             aria-label="open drawer"
             edge="start"
             onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
+            sx={{ mr: 2, display: { sm: "none" } }}
           >
             <MenuIcon />
           </IconButton>
@@ -96,11 +97,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           </Typography>
         </Toolbar>
       </AppBar>
-      <Box
-        component="nav"
-        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-        aria-label="mailbox folders"
-      >
+      <Box component="nav" sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }} aria-label="mailbox folders">
         {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
         <Drawer
           container={container}
@@ -111,8 +108,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             keepMounted: true, // Better open performance on mobile.
           }}
           sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
           }}
         >
           {drawer}
@@ -120,18 +117,15 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <Drawer
           variant="permanent"
           sx={{
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
           }}
           open
         >
           {drawer}
         </Drawer>
       </Box>
-      <Box
-        component="main"
-        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
-      >
+      <Box component="main" sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}>
         <Toolbar />
         {children}
       </Box>
