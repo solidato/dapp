b3ae1aa8cf361d2462ac29006f4311fa4298f2ae
Stefano Ceschi Berrini <stefano.ceschib@gmail.com>
2023-02-20 00:47:40 +0100

    feat: other cards fixes

--- components/ResolutionCard.tsx
+++ components/ResolutionCard.tsx
@@ -13,17 +13,22 @@ import Typography from "@mui/material/Typography";
 import ResolutionInfo from "@components/ResolutionInfo";
 import User from "@components/User";
 
+import useResolutionsAcl from "../hooks/useResolutionsAcl";
 import { RESOLUTION_STATES } from "../lib/resolutions/common";
 import { ResolutionEntityEnhanced } from "../types";
+import Countdown from "./Countdown";
 import Modal from "./Modal";
 import VotingWidget from "./VotingWidget";
 
 export default function ResolutionCard({ resolution }: { resolution: ResolutionEntityEnhanced }) {
   const [voteModalOpen, setVoteModalOpen] = useState(false);
+  const { acl } = useResolutionsAcl();
 
   const handleVote = () => {
     setVoteModalOpen(true);
   };
+
+  const canEdit = resolution.state === RESOLUTION_STATES.PRE_DRAFT && acl.canUpdate;
 
   return (
     <Card variant={resolution.state === RESOLUTION_STATES.VOTING ? "elevation" : "outlined"} elevation={12}>
@@ -53,7 +58,11 @@ export default function ResolutionCard({ resolution }: { resolution: ResolutionE
                 </Button>
               }
             >
-              You can now vote for this resolution
+              <Countdown
+                targetDate={resolution.resolutionTypeInfo.votingEnds as Date}
+                prefixLabel="Voting ends"
+                inline
+              />
             </Alert>
           )}
           {resolution.state === RESOLUTION_STATES.ENDED && (
@@ -64,23 +73,31 @@ export default function ResolutionCard({ resolution }: { resolution: ResolutionE
           {resolution.state === RESOLUTION_STATES.REJECTED && (
             <Alert severity="error">This resolution has been rejected</Alert>
           )}
+          {resolution.state === RESOLUTION_STATES.PRE_DRAFT && (
+            <Alert severity="info">Resolution needs to be approved</Alert>
+          )}
+          {resolution.state === RESOLUTION_STATES.NOTICE && (
+            <Alert severity="info">
+              <Countdown
+                targetDate={resolution.resolutionTypeInfo.noticePeriodEnds as Date}
+                prefixLabel="Voting starts"
+                inline
+              />
+            </Alert>
+          )}
         </Box>
       </CardContent>
-      <CardActions sx={{ mt: 1 }}>
+      <CardActions sx={{ mt: 2, bgcolor: "grey.100", ['[data-mui-color-scheme="dark"] &']: { bgcolor: "grey.900" } }}>
         <Stack direction="row" alignItems="center" justifyContent="space-between" width="100%">
           <ResolutionInfo chipSize="small" resolution={resolution} hideState />
           <Button
             variant="outlined"
             color="primary"
             size="small"
-            href={
-              resolution.state === RESOLUTION_STATES.PRE_DRAFT
-                ? `/resolutions/${resolution.id}/edit`
-                : `/resolutions/${resolution.id}`
-            }
+            href={canEdit ? `/resolutions/${resolution.id}/edit` : `/resolutions/${resolution.id}`}
             LinkComponent={Link}
           >
-            View
+            {canEdit ? "Edit" : "View"}
           </Button>
         </Stack>
       </CardActions>
