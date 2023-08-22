b28b270e728a631458cdc40bcdb5dc8eeded00c9
Gianluca Donato <gian.dnt@gmail.com>
2023-08-22 12:35:23 +0200

    fix wallet network switch

--- components/CheckConnected.tsx
+++ components/CheckConnected.tsx
@@ -5,18 +5,21 @@ import { Alert, AlertTitle, Box, Button } from "@mui/material";
 
 import useUser from "@hooks/useUser";
 
+import { SUPPORTED_CHAINS } from "../pages/_app";
 import Section from "./Section";
 
 export default function CheckConnected({ fullWidth = false }: { fullWidth?: boolean }) {
   const { isConnected } = useAccount();
   const { user } = useUser();
-  const { open: openWeb3Modal } = useWeb3Modal();
+  const { open: openWeb3Modal, setDefaultChain } = useWeb3Modal();
 
   if (isConnected || !user?.isLoggedIn) {
     return null;
   }
 
   const Component = fullWidth ? Section : Box;
+
+  setDefaultChain(SUPPORTED_CHAINS[0]);
 
   return (
     <Component sx={{ mb: fullWidth ? 0 : 2 }}>
