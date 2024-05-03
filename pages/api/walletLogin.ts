1b4d14f9f07cc3fa59e4f88d4a2f9336930e6221
Gianluca Donato <gian.dnt@gmail.com>
2024-05-03 21:41:54 +0200

    fetch authUser from shareholders

--- pages/api/walletLogin.ts
+++ pages/api/walletLogin.ts
@@ -1,15 +1,14 @@
+import { eq } from "drizzle-orm";
 import { withIronSessionApiRoute } from "iron-session/next";
 import jwt from "jsonwebtoken";
 import { NextApiRequest, NextApiResponse } from "next";
 import { recoverMessageAddress } from "viem";
 
-import { getOdooCookie } from "@lib/getOdooCookie";
-import odooClient from "@lib/graphql/odoo";
-import { getUserByAddressQuery } from "@lib/graphql/queries/get-user-by-address.query";
 import { sessionOptions } from "@lib/session";
 import userFactory from "@lib/userFactory";
 
-import { OdooUser } from "../../types";
+import { db } from "../../drizzle";
+import { shareholders } from "../../schema/shareholders";
 
 // Login with Wallet
 const walletLoginRoute = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -26,47 +25,27 @@ const walletLoginRoute = async (req: NextApiRequest, res: NextApiResponse) => {
     }
   }
 
-  if (req.method === "PUT") {
-    const { signingToken, address, signature } = req.body as {
-      signingToken: string;
-      address: string;
-      signature: string;
-    };
-    const password = JSON.stringify({ signing_token: signingToken, signature });
-    const pwdB64 = Buffer.from(password, "utf8").toString("base64");
-    try {
-      const cookie = await getOdooCookie(address.toLowerCase(), pwdB64);
-      const data = await odooClient.query(cookie, getUserByAddressQuery, { address: address.toLowerCase() });
-      const userData = data.ResUsers[0] as OdooUser;
-      const user = userFactory({ ...userData, username: address.toLowerCase(), password: pwdB64, isLoggedIn: true });
-      req.session.cookie = cookie;
-      req.session.user = user;
-      await req.session.save();
-      return res.status(200).json(user);
-    } catch (error: any) {
-      if (error.isBoom) {
-        return res.status(error.output.statusCode).json(error.output.payload);
-      }
-      return res.status(500).json({ error: JSON.stringify(error) });
-    }
-  }
-
   if (req.method === "POST") {
     const { signingToken, signature } = req.body;
     try {
       const { message } = jwt.verify(signingToken, JWT_SECRET) as { message: string };
       const address = await recoverMessageAddress({ message, signature });
       // Check if the signer is in the Database!
-      const user = userFactory({
-        id: 1,
-        name: "Gianluca",
-        email: "gian.dnt@gmail.com",
-        ethereum_address: address,
+      const results = await db.query.shareholders.findMany({ where: eq(shareholders.ethAddress, address) });
+      if (!results.length) {
+        return res.status(401).json({ error: "User not found" });
+      }
+      const user = results[0];
+      const authUser = userFactory({
+        ...user,
+        ethereum_address: user.ethAddress,
+        name: `${user.firstName} ${user.lastName}`,
+        display_name: user.firstName,
         isLoggedIn: true,
       });
       const cookie = jwt.sign({ sub: address, role: "user" }, JWT_SECRET);
-      // req.session.cookie = cookie;
-      req.session.user = user;
+      req.session.cookie = cookie;
+      req.session.user = authUser;
       await req.session.save();
       return res.status(200).json(user);
     } catch (err) {
